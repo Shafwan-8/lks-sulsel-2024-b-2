7035a6f0f98bcf2680813c8d8d8964ad15509e9d
Wawanzz_ <arul.safwan73@gmail.com>
2024-07-27 12:08:55 +0700

    membuat relasi antar model user dan gallery

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Gallery extends Model
 {
@@ -16,4 +17,9 @@ class Gallery extends Model
         'user_id',
         'description'
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
